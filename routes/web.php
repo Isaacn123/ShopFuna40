58074d831b9e01b2a39725eef2d5812decd59ed0
isaac1212 <nsambai722@gmail.com>
2021-12-19 23:14:06 +0300

    web

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return \View::make('welcome');
+    return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
