e0e367d51c7ccc4dee4373d665906abc8466d4ec
isaac1212 <nsambai722@gmail.com>
2021-12-27 02:57:39 +0300

    sub carts

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -23,6 +23,7 @@ class ProductCollection extends JsonResource
             'total_price' => round((1 - ($this->discount/100)) * $this->price,2),
             'discount' => $this->discount,
             'imagePath' => $this->imagePath,
+            'subCategory_id' => $this->subCategory_id,
             'featured_image' => $this -> featured_image,
             'created_at' => $this->created_at,
             'ratings' =>$this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(),2) : 'no ratings',
