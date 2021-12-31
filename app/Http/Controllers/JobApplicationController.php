92fc64bc0ca9bc36af30bc367d35c9e2585a1f03
isaac1212 <nsambai722@gmail.com>
2021-12-31 17:32:51 +0300

    change save method

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreJobApplication;
+use App\Models\JobApplication;
 
 class JobApplicationController extends Controller
 {
@@ -38,44 +39,7 @@ class JobApplicationController extends Controller
     public function store(StoreJobApplication $request)
     {
         //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(StoreJobApplication $request, $id)
-    {
-        //
-
-      
-    
-          
         $jobapplication = new JobApplication();
 
         $jobapplication ->firstName = $request->firstName;
@@ -114,6 +78,42 @@ class JobApplicationController extends Controller
         ], 200);
 
         return $response;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreJobApplication $request, $id)
+    {
+        //
 
     }
 
