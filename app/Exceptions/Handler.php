77f8bee6b2d7510f4d5076b63bcdb6b38dbd338f
isaac1212 <nsambai722@gmail.com>
2022-01-07 16:18:20 +0300

    update 1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -39,9 +39,9 @@ class Handler extends ExceptionHandler
         //   dd($e);
         });
 
-        if ($exception instanceof ValidationException && $request->expectsJson()) {
-            return response()->json(['message' => 'The given data was invalid.', 'errors' => $exception->validator->getMessageBag()], 422);
-        }
+        // if ($exception instanceof ValidationException && $request->expectsJson()) {
+        //     return response()->json(['message' => 'The given data was invalid.', 'errors' => $exception->validator->getMessageBag()], 422);
+        // }
 
 
     }
