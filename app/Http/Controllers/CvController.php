89ddc7eb2cdff230c78c1ddbccf822c13b3e887d
isaac1212 <nsambai722@gmail.com>
2022-01-08 16:33:54 +0300

    upload

--- app/Http/Controllers/CvController.php
+++ app/Http/Controllers/CvController.php
@@ -219,26 +219,26 @@ class CvController extends Controller
 
         if($cvs){
        
-            // $result =   cloudinary()->uploader->destroy("cv_application/Cv_Nsamaba1641644304.pdf");
-            // dd($result); 
+            $result =   cloudinary()->uploader->destroy("cv_application/Cv_Nsamaba1641644304");
+            dd($result); 
 
-            if(isset($request->pdf_file)){
-                $result = $request->pdf_file->storeOnCloudinaryAs('cv_application', $nameF);
-                $imagename = $result->getFileName();
-                $extension = $result->getExtension();
+            // if(isset($request->pdf_file)){
+            //     $result = $request->pdf_file->storeOnCloudinaryAs('cv_application', $nameF);
+            //     $imagename = $result->getFileName();
+            //     $extension = $result->getExtension();
         
-                $name = $imagename . "." . $extension;
-                $path = $result->getSecurePath();
-                $id   =  $result->getPublicId();
-                $cvs->pdf_file = $name;
-                $imageID = $result->getPublicId();
+            //     $name = $imagename . "." . $extension;
+            //     $path = $result->getSecurePath();
+            //     $id   =  $result->getPublicId();
+            //     $cvs->pdf_file = $name;
+            //     $imageID = $result->getPublicId();
     
     
-            }
+            // }
         }
 
      
-         $cvs->save();
+        //  $cvs->save();
     
 
         return [
