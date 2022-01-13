3553e81c0f779940856a1333d3258a4c5e4c5bf7
isaac1212 <nsambai722@gmail.com>
2022-01-14 01:40:00 +0300

    add id

--- app/Http/Controllers/BusinessAllSubCategories.php
+++ app/Http/Controllers/BusinessAllSubCategories.php
@@ -12,7 +12,7 @@ class BusinessAllSubCategories extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
         //
         return BussinessSubCategory::where('category_name', $id)->get();
