f852564a4bae7e91e6df0be0119e4fa784a66fab
isaac1212 <nsambai722@gmail.com>
2022-01-22 18:59:54 +0300

    request

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Banner;
 use Illuminate\Http\Request;
-use App\Http\requests\StoreBannerRequest;
+use App\Http\Requests\StoreBannerRequest;
 
 class BannerController extends Controller
 {
