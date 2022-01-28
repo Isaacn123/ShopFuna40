7f1ed831f104f4aeb197e1ebaa8f49c9be9ba615
isaac1212 <nsambai722@gmail.com>
2022-01-28 20:19:12 +0300

    upda

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -220,19 +220,19 @@ class ProductController extends Controller
         $product ->phone = $request->phone;
         $product ->slug = $request-> slug;
         $product->related_products = $request->related_products; 
-        $product->featured_image = $request->featured_image; 
+        $product->featured_image = "request->featured_image"; 
 
-        if(isset($request->featured_image))
-        {
-            if($request->publicIdbanner != null){
-                if($request->publicIdbanner != "featured/no_featuredImage.jpg")
-                {
-                    // publicIdbanner
-                 cloudinary()->destroy($request->publicIdbanner);
-                }
-            }
+        // if(isset($request->featured_image))
+        // {
+        //     if($request->publicIdbanner != null){
+        //         if($request->publicIdbanner != "featured/no_featuredImage.jpg")
+        //         {
+        //             // publicIdbanner
+        //          cloudinary()->destroy($request->publicIdbanner);
+        //         }
+        //     }
           
-        };
+        // };
 
         // $nameF = "Product_" . time();
         // if(isset($request->featured_image)){
