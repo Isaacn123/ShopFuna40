f2a502e6bf5997357f2b5cb6a55ca73f90be01a2
isaacn123 <nsambai72@gmail.com>
2022-02-02 01:19:29 +0300

    message

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,13 +1,16 @@
 @component('mail::message')
-Hello **{{$name}}**,
 # Welcome to Funa Akatale!
 
-Thank you for taking time to signing up with us.
+Hi *{{$name}}**,
+
+Thanks you for taking time to signing up on Funa Akatale. Your email is #{{email}}.
+You can now access your account to view or edit your account,orders, change your password, and more.
 
 @component('mail::button', ['url' => 'http://www.funa-akatale.com'])
 visit us
 @endcomponent
 
 Thank You,<br>
+We look forward to sending you soon.<br>
 {{ config('app.name') }}
 @endcomponent
