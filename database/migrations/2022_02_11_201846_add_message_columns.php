dbc11cd4cbdd6a6b869e2320bbdb88da47c45357
isaacn123 <nsambai72@gmail.com>
2022-02-11 23:46:25 +0300

    add colum

---
+++ database/migrations/2022_02_11_201846_add_message_columns.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMessageColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('messages', function (Blueprint $table) {
+
+            $table->string('user_id')->nullable();
+            $table->string('user_email')->nullable();
+            $table->string('product_id')->nullable();
+            $table->string('product_name')->nullable();
+            $table->string('product_image')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
