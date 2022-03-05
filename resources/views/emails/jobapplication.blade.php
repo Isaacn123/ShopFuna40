f0f0ea20bd25588db922e6049e885318e53af504
isaacn123 <nsambai72@gmail.com>
2022-03-05 20:07:21 +0300

    message

--- resources/views/emails/jobapplication.blade.php
+++ resources/views/emails/jobapplication.blade.php
@@ -4,12 +4,10 @@ From: *{{$email}}*
 
 Subject: Application for the post of an *{{$job}}*
 
-Dear Sir/Madam
-
-This email is in reference to your advertisment on FUNA AKATALE APP.<br>
-{{$message}}.
-
-I thank you for your time and condideration. i look forward for an opportunity to to sit for an interview with you.I can be contacted at {{$phone}}
+Dear Sir/Madam<br>
+This email is in reference to your advertisment on FUNA AKATALE APP.
+{{$message}}.<br>
+I thank you for your time and consideration. i look forward for an opportunity to to sit for an interview with you.I can be contacted at {{$phone}}
 
 <!-- @component('mail::button', ['url' => ''])
 Button Text
