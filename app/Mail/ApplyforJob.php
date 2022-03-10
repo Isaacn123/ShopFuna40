7ea3767f01091d82ec00ff13acee3502fa277999
isaacn123 <nsambai72@gmail.com>
2022-03-10 09:50:25 +0300

    d

--- app/Mail/ApplyforJob.php
+++ app/Mail/ApplyforJob.php
@@ -55,7 +55,7 @@ class ApplyforJob extends Mailable
             'job' => $data['job_title'],
 
         ])
-        ->attach("https://res.cloudinary.com/ivhfizons/image/upload/v1646670999/uploads/vba9gk8xa2nncyeqfacj.pdf"
+        ->attach($data['path']."/".$data['file']);
         // ->attach(asset($data['file']), ['mime' => 'application/pdf']);"
         // ->attachData("https://res.cloudinary.com/ivhfizons/image/upload/v1639074703/uploads/poo7te9essqljbrqbjzz".".pdf",'resume.pdf',
         //  [
